2c9900bd089aba77790f64807bea29f6cd904130
Stuart Bailey <stuartjbailey@googlemail.com>
2012-04-09 16:10:43 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '84dc583ce5c76e8230986976678c445de50a78fd7730414e56ea8133012e63b31a493db7b15ac2c0665d8345009c5b111cb2f195ff9420cfb4fce5f6d90e860a'
---
+++ db/migrate/20120408104658_add_password_digest_to_users_password_digest.rb
@@ -0,0 +1,4 @@
+class AddPasswordDigestToUsersPasswordDigest < ActiveRecord::Migration
+  def change
+  end
+end
